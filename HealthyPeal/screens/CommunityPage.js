98f0c45b001d63fd3b209a9ab76aad56aaacc563
Asha <asha17@brown.edu>
2020-06-24 17:50:49 -0700

    adding image i think

--- HealthyPeal/screens/CommunityPage.js
+++ HealthyPeal/screens/CommunityPage.js
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  StatusBar,
+  ScrollView,
+  Image
+} from "react-native";
+import { Icon } from 'react-native-eva-icons';
+import CommunityModule from "../components/CommunityModule";
+
 import { Layout, Text, Button } from '@ui-kitten/components';
+import COLORS from '../Colors';
 
 export default function CommunityPage({ navigation }) {
     return (
-      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-       <Button
-          size="medium"
-          onPress={() =>
-            navigation.navigate('Insights', { name: 'Jane' })
-          }>
-            Go to Jane's profile
-          </Button>
-    </Layout>
+      <SafeAreaView style={{ flex: 1}}>
+        <View style={{flex: 1}}>
+          <View style={{height:100, backgroundColor: COLORS.primary,
+          borderBottomWidth: 1, borderBottomColor: COLORS.lightaccent}}>
+            <View style={{flexDirection:"row", padding: 10,
+            backgroundColor:COLORS.primary, marginHorizontal: 30,
+            shadowOffset: { width: 0, height: 0},
+            shadowOpacity: 0.2,
+            elevation: 1,
+            marginTop: 40
+            }}>
+              <Icon name='search-outline' width={24} height={24} fill={COLORS.lightaccent}/>
+              <TextInput
+              placeholder="Try Meditation"
+              placeholderTextColor={COLORS.lightaccent}
+              style={{flex: 1, fontWeight: "700", backgroundColor:COLORS.primary}}/>
+            </View>
+          </View>
+          <ScrollView
+            scrollEventThrottle={16}
+          >
+            <View style={{flex:1, backgroundColor:COLORS.primary, paddingTop: 20}}>
+              <Text style={{color:COLORS.darkaccent, fontSize:24, fontWeight:"700", paddingHorizontal:20}}>
+                What can we help you find?
+              </Text>
+              <View style={{height: 130, marginTop: 20}}>
+                <ScrollView>
+                    <CommunityModule imageURI={require("../assets/")}
+                    />
+                </ScrollView>
+              </View>
+            </View>
+          </ScrollView>
+    </View>
+    </SafeAreaView>
     )
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
