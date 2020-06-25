c2a13e9acbf100873cc800019062a848e0f63e1a
Asha <asha17@brown.edu>
2020-06-24 18:51:19 -0700

    insight and community page designed

--- HealthyPeal/screens/CommunityPage.js
+++ HealthyPeal/screens/CommunityPage.js
@@ -7,17 +7,19 @@ import {
   Platform,
   StatusBar,
   ScrollView,
-  Image
+  Image,
+  Dimensions,
+  Animated
 } from "react-native";
 import { Icon } from 'react-native-eva-icons';
-import CommunityModule from "../components/CommunityModule";
-
 import { Layout, Text, Button } from '@ui-kitten/components';
 import COLORS from '../Colors';
 
+const {height, width} = Dimensions.get("window")
+
 export default function CommunityPage({ navigation }) {
     return (
-      <SafeAreaView style={{ flex: 1}}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary}}>
         <View style={{flex: 1}}>
           <View style={{height:100, backgroundColor: COLORS.primary,
           borderBottomWidth: 1, borderBottomColor: COLORS.lightaccent}}>
@@ -32,36 +34,143 @@ export default function CommunityPage({ navigation }) {
               <TextInput
               placeholder="Try Meditation"
               placeholderTextColor={COLORS.lightaccent}
-              style={{flex: 1, fontWeight: "700", backgroundColor:COLORS.primary}}/>
+              style={{flex: 1, fontWeight: "700", backgroundColor:COLORS.primary}}
+              />
             </View>
           </View>
           <ScrollView
             scrollEventThrottle={16}
           >
+                        <Animated.View
+            style={{flexDirection: "row", marginHorizontal: 20, postion: "relative", top: 10, color: COLORS.primary}}>
+              <View style={{minHeight: 40, minWidth: 40, padding: 5,
+              backgroundColor: COLORS.primary, borderWidth: 0.2,
+              borderRadius: 2, borderColor: COLORS.lightaccent, marginLeft: 5, marginRight: 5}}>
+                <Text style={{fontWeight: "700", fontSize: 14, color: COLORS.lightaccent}}> Local </Text>
+              </View>
+              <View style={{minHeight: 40, minWidth: 40, padding: 5,
+              backgroundColor: COLORS.primary, borderWidth: 0.2,
+              borderRadius: 2}}>
+                <Text style={{fontWeight: "700", fontSize: 14, color: COLORS.lightaccent}}> Global </Text>
+              </View>
+            </Animated.View>
             <View style={{flex:1, backgroundColor:COLORS.primary, paddingTop: 20}}>
               <Text style={{color:COLORS.darkaccent, fontSize:24, fontWeight:"700", paddingHorizontal:20}}>
                 What can we help you find?
               </Text>
               <View style={{height: 130, marginTop: 20}}>
-                <ScrollView>
-                    <CommunityModule imageURI={require
-                    ("../assets/HealthyPeal/assets/becca-tapert--A_Sx8GrRWg-unsplash.jpg")}
-                    name="Home"
-                    />
-                    <CommunityModule imageURI={require
-                    ("../assets/HealthyPeal/assets/becca-tapert--A_Sx8GrRWg-unsplash.jpg")}
-                    name="Land"
-                    />
-                    <CommunityModule imageURI={require
-                    ("../assets/HealthyPeal/assets/becca-tapert--A_Sx8GrRWg-unsplash.jpg")}
-                    name="Sea"
-                    />
+                <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                >
+                  <View style={{height: 130, width: 130,
+                  marginLeft: 20, borderWidth: 0.5, borderColor: COLORS.lightaccent}}>
+                    <View style={{flex: 2}}>
+                      <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+                      style={{flex: 1, height : null, width: null, resizeMode: "cover"}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 10, paddingTop: 10 }}>
+                      <Text>Home</Text>
+                    </View>
+                  </View>
+                  <View style={{height: 130, width: 130,
+                  marginLeft: 20, borderWidth: 0.5, borderColor: COLORS.lightaccent}}>
+                    <View style={{flex: 2}}>
+                      <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+                      style={{flex: 1, height : null, width: null, resizeMode: "cover"}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 10, paddingTop: 10 }}>
+                      <Text>Away</Text>
+                    </View>
+                  </View>
+                  <View style={{height: 130, width: 130,
+                  marginLeft: 20, borderWidth: 0.5, borderColor: COLORS.lightaccent}}>
+                    <View style={{flex: 2}}>
+                      <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+                      style={{flex: 1, height : null, width: null, resizeMode: "cover"}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 10, paddingTop: 10 }}>
+                      <Text>Near</Text>
+                    </View>
+                  </View>
                 </ScrollView>
               </View>
-            </View>
-          </ScrollView>
+              <View style={{marginTop: 40, paddingHorizontal: 20}}>
+                <Text style={{color: COLORS.darkaccent, fontSize: 24, fontWeight: "700"}}>
+                  Introducing Peal Community
+                </Text>
+                <Text style={{color: COLORS.darkaccent, fontWeight: "300", marginTop: 10}}>
+                 A space for compassionate conversation
+                </Text>
+                <View style={{width:width-40, height:240, marginTop: 20}}>
+                <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+                      style={{flex: 1, height : null, width: null, resizeMode: "cover",
+                    borderRadius: 5, borderWidth: 1,
+                  borderColor: COLORS.lightaccent}}
+                      />
+                </View>
+              </View>
+  <View style={{marginTop: 40}}>
+      <Text style={{fontSize: 24, fontWeight: "700",
+    paddingHorizontal: 20}}>
+      Homes around the world
+    </Text>
+    <View style={{paddingHorizontal: 20, marginTop: 20, flexDirection: "row", flexWrap: "wrap",
+  justifyContent: "space-between"}}>
+      <View style={{width:width/2 - 30, height: width/2 - 30, borderWidth:0.5, borderColor:COLORS.lightaccent}}>
+        <View style={{flex: 1}}>
+        <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+          style={{flex: 1, height : null, width: null, resizeMode: "cover",
+          }}/>
+        </View>
+        <View style={{flex: 1, alignItems:"flex-start", justifyContent: "space-evenly", paddingLeft: 10, backgroundColor:COLORS.lightaccent}}>
+          <Text style={{fontSize: 14, color: COLORS.primary}}>Disordered Eating</Text>
+          <Text style={{fontSize: 12, fontWeight: "bold", color: COLORS.primary}}>100 members</Text>
+        </View>
+      </View>
+      <View style={{width:width/2 - 30, height: width/2 - 30, borderWidth:0.5, borderColor:COLORS.lightaccent}}>
+        <View style={{flex: 1}}>
+        <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+          style={{flex: 1, height : null, width: null, resizeMode: "cover",
+          }}/>
+        </View>
+        <View style={{flex: 1, alignItems:"flex-start", justifyContent: "space-evenly", paddingLeft: 10, backgroundColor:COLORS.lightaccent}}>
+          <Text style={{fontSize: 14, color: COLORS.primary}}>Disordered Eating</Text>
+          <Text style={{fontSize: 12, fontWeight: "bold", color: COLORS.primary}}>100 members</Text>
+        </View>
+      </View>
+      <View style={{width:width/2 - 30, height: width/2 - 30, borderWidth:0.5, borderColor:COLORS.lightaccent}}>
+        <View style={{flex: 1}}>
+        <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+          style={{flex: 1, height : null, width: null, resizeMode: "cover",
+          }}/>
+        </View>
+        <View style={{flex: 1, alignItems:"flex-start", justifyContent: "space-evenly", paddingLeft: 10, backgroundColor:COLORS.lightaccent}}>
+          <Text style={{fontSize: 14, color: COLORS.primary}}>Disordered Eating</Text>
+          <Text style={{fontSize: 12, fontWeight: "bold", color: COLORS.primary}}>100 members</Text>
+        </View>
+      </View>
+      <View style={{width:width/2 - 30, height: width/2 - 30, borderWidth:0.5, borderColor:COLORS.lightaccent}}>
+        <View style={{flex: 1}}>
+        <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+          style={{flex: 1, height : null, width: null, resizeMode: "cover",
+          }}/>
+        </View>
+        <View style={{flex: 1, alignItems:"flex-start", justifyContent: "space-evenly", paddingLeft: 10, backgroundColor:COLORS.lightaccent}}>
+          <Text style={{fontSize: 14, color: COLORS.primary}}>Disordered Eating</Text>
+          <Text style={{fontSize: 12, fontWeight: "bold", color: COLORS.primary}}>100 members</Text>
+        </View>
+      </View>
     </View>
-    </SafeAreaView>
+    </View>
+    </View>
+</ScrollView>
+
+</View>
+</SafeAreaView>
     )
 }
 
--- HealthyPeal/screens/HelloWorldPage.js
+++ HealthyPeal/screens/HelloWorldPage.js
@@ -1,37 +1,152 @@
-import React from 'react';
-import { StyleSheet } from "react-native";
+import React, {Component} from 'react';
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  StatusBar,
+  ScrollView,
+  Image,
+  Dimensions
+} from "react-native";
+import { Icon } from 'react-native-eva-icons';
 import { Layout, Text, Button } from '@ui-kitten/components';
-import COLORS from "../Colors";
+import COLORS from '../Colors';
 
-export default function HelloWorldApp({ navigation }) {
+const {height, width} = Dimensions.get("window")
+
+export default function CommunityPage({ navigation }) {
     return (
-      <Layout style={styles.container}>
-      <Text style={styles.title} category='h1'>Home</Text>
-       <Button
-          size="medium"
-          style={styles.button1}
-          onPress={() =>
-            navigation.navigate('Insights', { name: 'Jane' })
-          }>
-            Go to Jane's profile
-          </Button>
-    </Layout>
+      <SafeAreaView style={{ flex: 1}}>
+        <View style={{flex: 1, marginTop: 30}}>
+          <ScrollView
+            scrollEventThrottle={16}
+          >
+            <View style={{flex:1, backgroundColor:COLORS.primary, paddingTop: 20}}>
+              <Text style={{color:COLORS.darkaccent, fontSize:32, fontWeight:"700", paddingHorizontal:20}}>
+                Your Daily Steps
+              </Text>
+              <View style={{height: 130, marginTop: 20}}>
+                <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                >
+                  <View style={{height: 130, width: 130,
+                  marginLeft: 20, borderWidth: 0.5, borderColor: COLORS.lightaccent}}>
+                    <View style={{flex: 2}}>
+                      <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+                      style={{flex: 1, height : null, width: null, resizeMode: "cover"}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 10, paddingTop: 10 }}>
+                      <Text>Home</Text>
+                    </View>
+                  </View>
+                  <View style={{height: 130, width: 130,
+                  marginLeft: 20, borderWidth: 0.5, borderColor: COLORS.lightaccent}}>
+                    <View style={{flex: 2}}>
+                      <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+                      style={{flex: 1, height : null, width: null, resizeMode: "cover"}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 10, paddingTop: 10 }}>
+                      <Text>Away</Text>
+                    </View>
+                  </View>
+                  <View style={{height: 130, width: 130,
+                  marginLeft: 20, borderWidth: 0.5, borderColor: COLORS.lightaccent}}>
+                    <View style={{flex: 2}}>
+                      <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+                      style={{flex: 1, height : null, width: null, resizeMode: "cover"}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 10, paddingTop: 10 }}>
+                      <Text>Near</Text>
+                    </View>
+                  </View>
+                </ScrollView>
+              </View>
+              <View style={{marginTop: 40, paddingHorizontal: 20}}>
+                <Text style={{color: COLORS.darkaccent, fontSize: 24, fontWeight: "700"}}>
+                  Introducing Peal Community
+                </Text>
+                <Text style={{color: COLORS.darkaccent, fontWeight: "300", marginTop: 10}}>
+                 A space for compassionate conversation
+                </Text>
+                <View style={{width:width-40, height:240, marginTop: 20}}>
+                <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+                      style={{flex: 1, height : null, width: null, resizeMode: "cover",
+                    borderRadius: 5, borderWidth: 1,
+                  borderColor: COLORS.lightaccent}}
+                      />
+                </View>
+              </View>
+  <View style={{marginTop: 40}}>
+      <Text style={{fontSize: 24, fontWeight: "700",
+    paddingHorizontal: 20}}>
+      Homes around the world
+    </Text>
+    <View style={{paddingHorizontal: 20, marginTop: 20, flexDirection: "row", flexWrap: "wrap",
+  justifyContent: "space-between"}}>
+      <View style={{width:width/2 - 30, height: width/2 - 30, borderWidth:0.5, borderColor:COLORS.lightaccent}}>
+        <View style={{flex: 1}}>
+        <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+          style={{flex: 1, height : null, width: null, resizeMode: "cover",
+          }}/>
+        </View>
+        <View style={{flex: 1, alignItems:"flex-start", justifyContent: "space-evenly", paddingLeft: 10, backgroundColor:COLORS.lightaccent}}>
+          <Text style={{fontSize: 14, color: COLORS.primary}}>Disordered Eating</Text>
+          <Text style={{fontSize: 12, fontWeight: "bold", color: COLORS.primary}}>100 members</Text>
+        </View>
+      </View>
+      <View style={{width:width/2 - 30, height: width/2 - 30, borderWidth:0.5, borderColor:COLORS.lightaccent}}>
+        <View style={{flex: 1}}>
+        <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+          style={{flex: 1, height : null, width: null, resizeMode: "cover",
+          }}/>
+        </View>
+        <View style={{flex: 1, alignItems:"flex-start", justifyContent: "space-evenly", paddingLeft: 10, backgroundColor:COLORS.lightaccent}}>
+          <Text style={{fontSize: 14, color: COLORS.primary}}>Disordered Eating</Text>
+          <Text style={{fontSize: 12, fontWeight: "bold", color: COLORS.primary}}>100 members</Text>
+        </View>
+      </View>
+      <View style={{width:width/2 - 30, height: width/2 - 30, borderWidth:0.5, borderColor:COLORS.lightaccent}}>
+        <View style={{flex: 1}}>
+        <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+          style={{flex: 1, height : null, width: null, resizeMode: "cover",
+          }}/>
+        </View>
+        <View style={{flex: 1, alignItems:"flex-start", justifyContent: "space-evenly", paddingLeft: 10, backgroundColor:COLORS.lightaccent}}>
+          <Text style={{fontSize: 14, color: COLORS.primary}}>Disordered Eating</Text>
+          <Text style={{fontSize: 12, fontWeight: "bold", color: COLORS.primary}}>100 members</Text>
+        </View>
+      </View>
+      <View style={{width:width/2 - 30, height: width/2 - 30, borderWidth:0.5, borderColor:COLORS.lightaccent}}>
+        <View style={{flex: 1}}>
+        <Image source={{uri: "https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"}}
+          style={{flex: 1, height : null, width: null, resizeMode: "cover",
+          }}/>
+        </View>
+        <View style={{flex: 1, alignItems:"flex-start", justifyContent: "space-evenly", paddingLeft: 10, backgroundColor:COLORS.lightaccent}}>
+          <Text style={{fontSize: 14, color: COLORS.primary}}>Disordered Eating</Text>
+          <Text style={{fontSize: 12, fontWeight: "bold", color: COLORS.primary}}>100 members</Text>
+        </View>
+      </View>
+    </View>
+    </View>
+    </View>
+</ScrollView>
+
+</View>
+</SafeAreaView>
     )
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 0,
-    backgroundColor: COLORS.primary,
-    },
-  title: {
-    marginTop: 100,
-    color: COLORS.darkaccent,
-    marginHorizontal: 20,
-  },
-  button1: {
-    marginTop: 30,
-    marginHorizontal: 50
-  },
-});
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
