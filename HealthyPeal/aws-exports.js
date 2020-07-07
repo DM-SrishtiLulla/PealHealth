a16909155198f33d7656682f8f0de6afb65b4ed5
Srishti <srishti_lulla@brown.edu>
2020-07-06 20:22:32 -0400

    removed api

--- HealthyPeal/aws-exports.js
+++ HealthyPeal/aws-exports.js
@@ -7,11 +7,7 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_InJQcYIev",
     "aws_user_pools_web_client_id": "4am4vuc6m346v78j52mrfjhgpg",
-    "oauth": {},
-    "aws_appsync_graphqlEndpoint": "https://otbrf533xzdzna25nqmcvew6ee.appsync-api.us-east-1.amazonaws.com/graphql",
-    "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-twg55f6vmbef5hfmi67szudlpm"
+    "oauth": {}
 };
 
 
