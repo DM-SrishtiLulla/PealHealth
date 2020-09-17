ab739391697fd579e5083f038e10d225714d8edd
Srishti <srishti_lulla@brown.edu>
2020-09-17 16:55:05 -0400

    Got them extra items if they didn't fill out enough!!

--- HealthyPeal/screens/HelloWorldPage.js
+++ HealthyPeal/screens/HelloWorldPage.js
@@ -15,7 +15,7 @@ import Carousel from "./Carousel";
 import { dummyData } from "./Data.js";
 import { useEffect, useState } from 'react'
 import { API, graphqlOperation, Auth } from 'aws-amplify'
-import { listUserInsightss, getUserInfo, getInsight } from './../src/graphql/queries';
+import { listUserInsightss, getUserInfo, getInsight, listInsights } from './../src/graphql/queries';
 import COLORS from "../Colors";
 
 
@@ -43,38 +43,24 @@ export default function HelloWorldPage({ navigation }) {
       let userInsights = [];
       const insight = await API.graphql(graphqlOperation(listUserInsightss, {filter: {and: {userID: {eq: "scoconut26"}, status: {eq: "new"}}}}))
       const items = (insight.data.listUserInsightss.items)
-
+      console.log(items.length)
       const shuffled = items.sort(() => 0.5 - Math.random());
       let selected = shuffled.slice(0, 3);
-      //setInsights(selected)
       let actualData;
       for (const ins in selected) {
         actualData = selected[ins].insight
         userInsights.push(actualData)
       }
-      setInsights(userInsights)
-      //console.log(items)
-      /*const insightI = await API.graphql(graphqlOperation(getUserInfo, {id: user}))
-      const item = (insightI.data.getUserInfo.Insights.items)
-      //const letstry = await API.graphql(graphqlOperation)
-      let insightID;
-      let insightFull;
-      let actualData;
-      //console.log(item);
-      for (const i in item) {
-        insightID = item[i].insightID
-        insightFull = await API.graphql(graphqlOperation(getInsight, {id: insightID}))
-        actualData = insightFull.data.getInsight
-        userInsights.push(actualData)
+      const extraNeeded = 3 - items.length;
+      if (extraNeeded > 0) {
+        const extraInsights = await API.graphql(graphqlOperation(listInsights, {limit: extraNeeded}))
+        const extraItems = (extraInsights.data.listInsights.items)
+        console.log(extraItems)
+        for (ei in extraItems) {
+          userInsights.push(extraItems[ei])
+        }
       }
-      setInsights(userInsights)*/
-      /*const insightData = await API.graphql(graphqlOperation(listInsights))
-      const insights = insightData.data.listInsights.items
-      console.log(insights)
-      /*setInsights(insights)
-      const shuffled = insights.sort(() => 0.5 - Math.random());
-      let selected = shuffled.slice(0, 3);
-      setInsights(selected)*/
+      setInsights(userInsights)
     } catch (err) { console.log('error fetching insightsjas') }
   }
 
