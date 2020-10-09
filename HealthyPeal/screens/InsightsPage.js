51d1b5e72b7cb15348a52ca58adfc67f57dabeb0
Srishti <srishti_lulla@brown.edu>
2020-10-08 23:58:40 -0400

    Got goals and insights on InsightsPage.js

--- HealthyPeal/screens/InsightsPage.js
+++ HealthyPeal/screens/InsightsPage.js
@@ -2,26 +2,59 @@ import { API, graphqlOperation } from 'aws-amplify'
 import { listGoals, listInsights } from './../src/graphql/queries'
 import { createInsight } from './../src/graphql/mutations'
 import React, { useEffect, useState } from 'react'
+import COLORS from "../Colors";
+
 import {
-  View, Text, StyleSheet, TextInput, Button, Image
+  View, Text, StyleSheet, TextInput, Button, Image, FlatList, StatusBar, SafeAreaView, TouchableOpacity
 } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
+import { useIsFocused } from '@react-navigation/native';
 
+const Item = ({ item, onPress, style }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+    <Text style={styles.title}>{item.GoalText}</Text>
+  </TouchableOpacity>
+);
 
 const InsightsPage = () => {
   const [insights, setInsights] = useState([])
+  const [goals, setGoals] = useState([])
+  const isFocused = useIsFocused();
+  const [selectedId, setSelectedId] = useState(null);
 
   useEffect(() => {
-    fetchInsights()
-  }, [])
+    if (isFocused) {
+      fetchGoals()
+      fetchInsights()
+    }
+  }, [isFocused]);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        style={{ backgroundColor }}
+      />
+    );
+  }; 
 
   async function fetchInsights() {
     try {
       const insightData = await API.graphql(graphqlOperation(listInsights))
       const insights = insightData.data.listInsights.items
-      console.log(insights)
       setInsights(insights)
     } catch (err) { console.log('error fetching insights') }
+  }
+
+  async function fetchGoals() {
+    try {
+      const goalData = await API.graphql(graphqlOperation(listGoals))
+      const goals = goalData.data.listGoals.items
+      setGoals(goals)
+    } catch (err) { console.log('error fetching goals') }
   }
 
   /*async function addInsight() {
@@ -34,6 +67,7 @@ const InsightsPage = () => {
       console.log('error creating goal:', err)
     }
   }*/
+
 
   return (
     <ScrollView>
@@ -51,6 +85,14 @@ const InsightsPage = () => {
             placeholder="ImageLink"
         />
         <Button title="Create goal" onPress={addGoal} />  */}
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={goals}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            extraData={selectedId}
+          />
+        </SafeAreaView>
         {
             insights.map((insight, index) => (
             <View key={insight.ID ? insight.ID : index} style={styles.identity}>
@@ -68,7 +110,19 @@ const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', padding: 20 },
   identity: {  marginBottom: 15 },
   input: { height: 50, backgroundColor: '#ddd', marginBottom: 10, padding: 8 },
-  identityTitle: { fontSize: 18 }
+  identityTitle: { fontSize: 18 },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
 })
 
 export default InsightsPage
